ca2411b8ed0f18517c2518fe3119720342e71b2d
Margot <Margot@MacBook-Pro-de-Fabien-2.local>
2019-05-03 16:10:14 +0200

    hierarchisation data for donut

--- src/components/ChartAssemblee.vue
+++ src/components/ChartAssemblee.vue
@@ -19,7 +19,7 @@
           :transform="text.transform"
           dy="0.35em"
         >{{text.display ? text.name : null}}</text>
-      </g> -->
+      </g>-->
     </svg>
   </div>
 </template>
@@ -35,20 +35,82 @@ import {
   quantize,
   interpolateRainbow,
   interpolate,
-  scaleLinear
+  scaleLinear,
+  dsv
 } from "d3";
 var TWEEN = require("@tweenjs/tween.js");
 
 export default {
   name: "ChartAssemblee",
   props: {
-    data: Object,
     width: {
       type: Number,
       default: 932
     }
+  },
+  data: function() {
+    return {
+      donutBudget: {
+        name: "Budget",
+        children: []
+      }
+    };
+  },
+  created: async function() {
+    let extractData = null;
+    let csv = dsv(";", "BudgetsAN_2012-2017.csv").then(data => {
+      extractData = data;
+    });
+    await csv;
+
+    let index_1 = -1;
+    let index_2 = -1;
+
+    extractData.forEach((elem, i) => {
+      let code = elem[""].split("/");
+      let name = elem.Libellé;
+      // elem["Budget 2012 avec budget supplémentaire SAIP mai 2012"]
+      console.log(code[0]);
+      if (code[0].length > 2) {
+        if (name.match(/[A-ZÀ-ÖŒ'\.\s]/g).length === name.length) {
+          console.log("name", name);
+          ++index_2;
+          if (!this.donutBudget.children[index_1].children) {
+            this.donutBudget.children[index_1]["children"] = [];
+          }
+          this.donutBudget.children[index_1].children.push({
+            name: name,
+            code: code,
+            children: []
+          });
+        } else {
+          console.log(index_1, index_2, this.donutBudget.children[index_1])
+          if (!this.donutBudget.children[index_1].children[index_2].children) {
+            console.log("je passe la")
+            this.donutBudget.children[index_1].children[index_2]["children"] = [];
+          }
+          this.donutBudget.children[index_1].children[index_2].children.push({
+            name: name,
+            code: code,
+            value: elem["Réalisé 2017"]
+          });
+        }
+      } else if (code[0].length === 2) {
+        ++index_1;
+        index_2 = -1;
+        if (!this.donutBudget.children) {
+          this.donutBudget["children"] = [];
+        }
+        this.donutBudget.children.push({
+          name: name,
+          code: code,
+          children: []
+        });
+      }
+    });
+    console.log(this.donutBudget);
+    // console.log(Object.values(this.dataBudget[0]))
   }
-  
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Assemblee.vue
+++ src/views/Assemblee.vue
@@ -1,34 +1,26 @@
 <template>
-    <div class="chart">
-        <a @click="refreshSize">Refresh Size</a>
-        <ChartAssemblee :data="data" :width="chartWidth" />
-    </div>
+  <div class="chart">
+    <a @click="refreshSize">Refresh Size</a>
+    <ChartAssemblee :width="chartWidth"/>
+  </div>
 </template>
 <script>
 import ChartAssemblee from "@/components/ChartAssemblee.vue";
-import {csv} from "d3";
-// import csvData from "../assets/BudgetsAN_2012-2017.csv";
-// const csvData = require('@/assets/BudgetsAN_2012-2017.csv')
 
 export default {
-    name: "donut",
-    components: {
-        ChartAssemblee
-    },
-    data: function() {
-        csv('BudgetsAN_2012-2017.csv').then((data) => {
-            console.log("data", data)
-            this.data = data
-        })
-        return {
-            data: null,
-            chartWidth: 950
-        };
-    },
-    methods: {
-        refreshSize: function() {
-            this.chartWidth = 900;
-        }
+  name: "donut",
+  components: {
+    ChartAssemblee
+  },
+  data: function() {
+    return {
+      chartWidth: 950
+    };
+  },
+  methods: {
+    refreshSize: function() {
+      this.chartWidth = 900;
     }
+  }
 };
 </script>
