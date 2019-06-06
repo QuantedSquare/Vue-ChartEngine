04443671cf156dbca04cfb7607ea1bfc9ed0ba15
GuySake <fabien.ungerer@gmail.com>
2019-06-06 17:11:51 +0200

    dev routes and favicon

--- src/router.js
+++ src/router.js
@@ -2,51 +2,55 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
+
+let devRoutes = [ // Charts
+    {
+        path: '/bars',
+        name: 'bars',
+        component: () => import('./views/charts/Bars.vue')
+    },
+    {
+        path: '/lines',
+        name: 'lines',
+        component: () => import('./views/charts/Lines.vue')
+    },
+    {
+        path: '/number',
+        name: 'number',
+        component: () => import('./views/charts/Number.vue')
+    },
+    // Story Player
+    {
+        path: '/player',
+        name: 'player',
+        component: () => import('./views/Player.vue')
+    }
+];
+
+let routes = [{
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+    // Real Stories
+    {
+        path: '/data-story/Boeing-2019',
+        name: 'Boeing-2019',
+        component: () => import('./views/stories/Boeing-2019.vue')
+    }
+]
 
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [{
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
-        },
-        // Charts
-        {
-            path: '/bars',
-            name: 'bars',
-            component: () => import('./views/charts/Bars.vue')
-        },
-        {
-            path: '/lines',
-            name: 'lines',
-            component: () => import('./views/charts/Lines.vue')
-        },
-        {
-            path: '/number',
-            name: 'number',
-            component: () => import('./views/charts/Number.vue')
-        },
-        // Story Player
-        {
-            path: '/player',
-            name: 'player',
-            component: () => import('./views/Player.vue')
-        },
-        // Real Stories
-        {
-            path: '/data-story/Boeing-2019',
-            name: 'Boeing-2019',
-            component: () => import('./views/stories/Boeing-2019.vue')
-        }
-    ]
+    routes: process.env.NODE_ENV == 'development' ? devRoutes.concat(routes) : routes
 })
