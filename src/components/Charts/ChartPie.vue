bebfcca0ee303faff4344e88e0d9a6f04d20f02a
GuySake <fabien.ungerer@gmail.com>
2019-07-10 11:54:01 +0200

    readability one pie chart

--- src/components/Charts/ChartPie.vue
+++ src/components/Charts/ChartPie.vue
@@ -1,19 +1,33 @@
 <template>
     <svg :viewBox="'0 0 ' + width + ' ' + height">
+        <!-- <defs>
+            <filter id="text-background" x="0" y="0" width="1" height="1">
+                <feFlood flood-color="black" />
+                <feComposite in="SourceGraphic" operator="xor" />
+            </filter>
+        </defs> -->
         <g :transform="display">
             <g :transform="center">
                 <template v-for="(path, i) in displayedPaths">
-                    <path :d="arc(path)" :fill="color(path.index)" class="arc-path"></path>
-                    <text v-if="moreThan25(path)" class="pie-label" :transform="arcCentroid(path)" text-anchor="middle">{{path.data.label}}</text>
+                    <g @mouseover="showHighlight(i)" @mouseleave="hideHighlight">
+                        <path :d="arc(path)" :fill="color(path.index)" class="arc-path"></path>
+                        <text v-if="moreThan25(path)" class="pie-label" :transform="arcCentroid(path)" text-anchor="middle">{{path.data.label}}</text>
+                    </g>
                 </template>
+                <text y="" class="highlight-val" text-anchor="middle">
+                    {{centerData.val}}
+                </text>
+                <text y="24" class="highlight-text" text-anchor="middle">
+                    {{centerData.text}}
+                </text>
             </g>
         </g>
     </svg>
 </template>
 <script>
-import { arc, pie, scaleOrdinal, quantize, interpolateRgbBasis } from 'd3'
+import { arc, pie, scaleOrdinal, quantize, interpolateRgbBasis, sum } from 'd3'
 
-let margin = { top: 40, right: 20, bottom: 40, left: 30 };
+let margin = { top: 40, right: 20, bottom: 40, left: 20 };
 
 let colorInterpolator = interpolateRgbBasis([
     'rgb(162, 255, 174)', 'rgb(12, 204, 249)',
@@ -45,24 +59,26 @@ export default {
         }
     },
     data: function() {
-        // let colorInterpolator = interpolateRgb('rgb(162, 255, 174)', 'rgb(12, 204, 249)');
-        // let colorInterpolator = interpolateRgb('rgb(172, 1, 207)', 'rgb(255, 18, 120)');
+        // console.log(_pie(this.data));
 
         let _arc = arc().innerRadius(this.radius() - 30)
             .outerRadius(this.radius());
 
-        let _pie = pie().value(d => d.y);
+        let outerArc = arc().innerRadius(this.radius())
+            .outerRadius(this.radius() + 30);
 
-        console.log(_pie(this.data));
+        let _pie = pie().value(d => d.y);
 
         let color = scaleOrdinal().domain(this.data.map(d => d.x))
             .range(quantize(t => colorInterpolator(t), this.data.length).reverse());
 
         return {
             arc: _arc,
+            outerArc: outerArc,
             pie: _pie,
             displayedPaths: _pie(this.data),
-            color: color
+            color: color,
+            centerData: { text: 'Total', val: this.total() }
         }
     },
     mounted: function() {
@@ -70,12 +86,10 @@ export default {
     },
     watch: {
         width: function() {
-            this.arc.innerRadius(this.radius() - 30)
-                .outerRadius(this.radius());
+            this.updateArcs();
         },
         height: function() {
-            this.arc.innerRadius(this.radius() - 30)
-                .outerRadius(this.radius());
+            this.updateArcs();
         },
         data: function() {
             this.displayedPaths = this.pie(this.data);
@@ -84,6 +98,13 @@ export default {
         }
     },
     methods: {
+        updateArcs: function() {
+            this.arc.innerRadius(this.radius() - 30)
+                .outerRadius(this.radius());
+
+            this.outerArc.innerRadius(this.radius())
+                .outerRadius(this.radius() + 30);
+        },
         _width: function() {
             return this.width - margin.left - margin.right;
         },
@@ -98,6 +119,19 @@ export default {
         },
         moreThan25: function(path) {
             return (path.endAngle - path.startAngle) > 0.25
+        },
+        showHighlight: function(pointIndex) {
+            console.log(pointIndex, this.data[pointIndex]);
+            this.centerData = {
+                val: this.data[pointIndex].y,
+                text: this.data[pointIndex].label
+            };
+        },
+        hideHighlight: function() {
+            this.centerData = { text: 'Total', val: this.total() }
+        },
+        total: function() {
+            return sum(this.data, d => d.y);
         }
     },
     computed: {
@@ -121,5 +155,15 @@ export default {
 .pie-label {
     /*opacity: 0.5;*/
     font-size: 12px;
+    /*color: white;*/
+}
+
+.highlight-val {
+    font-size: 48px;
+}
+
+.highlight-text {
+    font-size: 12px;
+    opacity: 0.7;
 }
 </style>
